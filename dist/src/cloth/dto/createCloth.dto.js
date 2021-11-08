083be7327a0a37873c3030a99a2a3f9dbb6253d1
jemin <jemin03120111@gmail.com>
2021-11-08 15:00:46 +0900

    db schema update2

--- dist/src/cloth/dto/createCloth.dto.js
+++ dist/src/cloth/dto/createCloth.dto.js
@@ -10,12 +10,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CreateClothDto = void 0;
-const openapi = require("@nestjs/swagger");
 const class_validator_1 = require("class-validator");
 class CreateClothDto {
-    static _OPENAPI_METADATA_FACTORY() {
-        return { top_bottom: { required: true, type: () => String }, short_long: { required: true, type: () => String }, color: { required: true, type: () => String }, material: { required: true, type: () => String } };
-    }
 }
 __decorate([
     (0, class_validator_1.IsNotEmpty)(),
